a9ca910eadecb2e853ca4bef0b94bc46d16b5897
Esra Yıldırım <esra_irem_37@hotmail.com>
2025-06-15 20:32:28 +0300

    İlk commit

---
+++ lib/views/locations.view/locations_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LocationsView extends StatelessWidget {
+  const LocationsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Konumlar'));
+  }
+}
